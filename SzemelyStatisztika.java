ba71c7e59be2dda7a02a23d81ceb96a497584e61
molnarbea <146650256+molnarbea@users.noreply.github.com>
2026-09-10 15:49:04 +0200

    Add files via upload

---
+++ SzemelyStatisztika.java
@@ -0,0 +1,90 @@
+package main;
+
+import java.util.Random;
+
+public class SzemelyStatisztika {
+
+    static Random rnd = new Random();
+
+    public static void main(String[] args) {
+
+        Program();
+
+    }
+
+    private static void Program() throws IllegalArgumentException {
+        String nev = "Anna";
+        int szulEv = 2003;
+        String[] koszones = {"Hello ", "Szia ", "Üdvözöllek "};
+
+        //System.out.println(koszontes(nev));
+        System.out.println(koszontes(koszones) + nev + "!");
+
+        System.out.println("Az életkorod: " + eletkor(szulEv));
+
+        if (nyugdijig(eletkor(szulEv)) < 1) {
+            System.out.println("Már nyugdíjas vagy!");
+        } else {
+            System.out.println("A nyugdíjig ennyi év van hátra: " + nyugdijig(eletkor(szulEv)));
+        }
+
+        if (!NevHosszabbMint2Karakter(nev)) {
+            throw new IllegalArgumentException("A név legyen hosszabb mint 2 karakter!");
+        }
+
+        if (!MegfeleloEletkor(eletkor(szulEv))) {
+            throw new IllegalArgumentException("Nem megfelel? születési év!");
+        }
+
+        if (!NyugdijigEvek(nyugdijig(eletkor(szulEv)))) {
+            throw new IllegalArgumentException("Nem megfelel? nyugdíjas évek!");
+        }
+    }
+
+    /*private static String koszontes(String nev){
+        String koszontes = "Hello %s!".formatted(nev);
+        return koszontes;
+    }*/
+    private static void koszontes(String nev) {
+        koszones(nev, false);
+    }
+
+    private static String koszontes(String[] koszones) {
+        return koszones[rnd.nextInt(koszones.length)];
+    }
+
+    private static void koszones(String nev, boolean keveres) {
+        String[] koszonesek = {"Szia", "Hello", "Jó napot"};
+        
+        String koszones=koszonesek[0];
+        if (keveres) {
+            int i = (int) (Math.random() * koszonesek.length);
+            koszones = koszonesek[i];
+        }
+        
+        System.out.println(koszones + "!" + nev + "!");
+        
+
+    }
+
+    private static int eletkor(int szulEv) {
+        return 2026 - szulEv;
+    }
+
+    private static int nyugdijig(int kor) {
+        return 65 - kor;
+    }
+
+    //tesztek
+    private static boolean NevHosszabbMint2Karakter(String nev) {
+        return nev.length() > 2;
+    }
+
+    private static boolean MegfeleloEletkor(int kor) {
+        return kor >= 0 && kor < 100;
+    }
+
+    private static boolean NyugdijigEvek(int evek) {
+        return evek < 66;
+    }
+}
